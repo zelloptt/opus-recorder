d1ee6521de0d66b46e80e26f713862db1d5c03b3
Mikhail Kiurshin <mk@zello.com>
2021-02-13 15:18:23 -0600

    inliner.pl

--- inliner.pl
+++ inliner.pl
@@ -1,5 +1,22 @@
-#!/bin/bash
-base64 < dist/encoderWorker.min.wasm > base64.txt
-sed -i.bak -f src/encoderWorker.inline.js << EOF
-s/BASE_64/$BASE_64/g
-EOF
+#!/perl
+my $wasmFilePath = 'dist-unminified/encoderWorker.wasm';
+my $base64FilePath = './base64.txt';
+
+my $encoderFilePath = './dist-unminified/encoderWorker.js';
+my $inlineEncoderFilePath = './src/encoderWorker.inline.js';
+
+`base64 $wasmFilePath > $base64FilePath`;
+open(BASE64, $base64FilePath) or die "Error: no file found.";
+(my $base64 = <BASE64>) =~ s/^\s+|\s+$//g;
+
+open(SOURCE, $encoderFilePath);
+open(RESULT, ">$inlineEncoderFilePath");
+while (<SOURCE>) {
+    if (/var wasmBinaryFile = 'encoderWorker.wasm';/) {
+        printf RESULT ("var wasmBinaryFile = dataURIPrefix + '%s';\n", $base64);
+    } else {
+        print RESULT $_;
+    }
+}
+
+unlink $base64FilePath;
